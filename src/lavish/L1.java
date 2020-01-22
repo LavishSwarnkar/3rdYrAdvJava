2be831890469e385eb648a6fc566708f77dd28b1
Lavish Swarnkar <lavishswarnkar@gmail.com>
2020-01-22 14:35:40 +0530

    initial commit

---
+++ src/lavish/L1.java
@@ -0,0 +1,16 @@
+package lavish;
+import java.sql.*;
+
+public class L1 {
+
+    public static void main(String[] args) throws SQLException{
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/lavish"
+                , "root"
+                , "");
+        Statement st = conn.createStatement();
+        st.executeUpdate("create table idiots(name varchar(10), id int)");
+        System.out.println("Success!");
+    }
+    
+}
